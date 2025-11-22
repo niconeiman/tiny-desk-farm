90d03f4f4a437075777f4a64d3cb1bbcc3a870fe
Nico Neiman <niconeiman@gmail.com>
2025-11-22 12:32:56 -0600

    feat: header

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Link from '@components/link.vue';
+
 const client = useSupabaseClient();
 const user = useSupabaseUser();
 
@@ -8,14 +10,22 @@ const logout = async () => {
 };
 </script>
 <template>
-  <nav
-    class="bg-primary-900 text-inverse p-4 text-center flex justify-between items-center"
-  >
-    <div>Tiny Desk Farm</div>
-    <div v-if="user">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/basics">Basics</NuxtLink>
-      <button @click="logout">Logout</button>
+  <nav class="bg-primary-900 text-inverse p-4 text-cente">
+    <div v-if="user" class="flex items-center justify-between">
+      <div class="flex items-center gap-2">
+        <Link to="/">Tiny Desk Farm</Link>
+        <Link to="/">Home</Link>
+        <Link to="/basics">Basics</Link>
+      </div>
+      <div class="flex items-center gap-2">
+        <div>Hi {{ user.email }}</div>
+        <button
+          @click="logout"
+          class="hover:text-primary-500 transition cursor-pointer"
+        >
+          Logout
+        </button>
+      </div>
     </div>
   </nav>
 </template>
---
+++ app/components/link.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  to: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="to"
+    class="hover:text-primary-500 transition cursor-pointer"
+    v-bind="$attrs"
+  >
+    <slot />
+  </NuxtLink>
+</template>
